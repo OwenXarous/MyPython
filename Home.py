23ca24d147404e68a105027f87dfdec42149c2ea
OwenXarous <owenshawza05@gmail.com>
2023-06-16 15:43:02 +0700

    .commit

--- Home.py
+++ Home.py
@@ -1,4 +1,10 @@
+from sklearn.neighbors import KNeighborsClassifier
 import streamlit as st
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import plotly.express as px
+import plotly.graph_objects as go
 
 st.header('Wongsatorn and Chutipong')
 st.header("Owen")
@@ -33,6 +39,24 @@ sp_wd = st.number_input("กรุณาเลือกข้อมูล sepal.
 
 
 if st.button("ทำนายผล"):
-    st.markdown("ทำนาย")
+    #st.markdown("ทำนาย")
+   dt = pd.read_csv("./data/iris.csv") 
+   X = dt.drop('variety', axis=1)
+   y = dt.variety
+   
+   Knn_model = KNeighborsClassifier(n_neighbors=3)
+   Knn_model.fit(X, y)   
+
+   x_input = np.array([[pt_len, pt_wd, sp_len, sp_wd]])
+   st.write(Knn_model.predict(x_input))
+   
+   out=Knn_model.predict(x_input)
+
+   if out[0] == 'Setosa':
+    st.image("./pic/Iris1.jpg")
+   elif: out[0] == 'Versicolor' 
+    st.image("./pic/Iris2.jpg")
+   else:
+    st.image("./pic/Iris3.jpg")
 else:
     st.write("ไม่ทำนาย")
